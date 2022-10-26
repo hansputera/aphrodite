8f99e9f2dade510a026933604f565628cd237393
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-10-26 15:17:46 +0000

    fix(token-validate): fix bot token length validation
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { handleUpdates } from './telegram';
 import type { EnvironmentVars } from './types';
+import { handleUpdates } from './telegram';
 import { verifyTelegramRequest } from './utils/telegram-verify';
 
 export default {
   async fetch(request: Request, env: EnvironmentVars) {
     if (
       typeof env.TELEGRAM_BOT_TOKEN !== 'string' ||
-      env.TELEGRAM_BOT_TOKEN.split(':').length !== -3
+      env.TELEGRAM_BOT_TOKEN.split(':').length !== 3
     ) {
       return new Response("Couldn't process this update!", {
-        status: 400,
+        status: 500,
       });
     } else if (request.method !== 'POST') {
       return new Response('incorrect method', { status: 405 });
